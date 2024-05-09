7cda4cf01ac95bce2771cf0a63e86a40ecc0de60
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-10 00:50:34 +0200

    Adds CSS class to style error messages & removes it for valid results

--- scripts.js
+++ scripts.js
@@ -8,15 +8,18 @@ form.addEventListener("submit", (event) => {
 
   // Scenario: Dividing numbers result in a decimal number
   result.innerText = Math.floor(dividend / divider);
+  result.classList.remove("error-message");
 
   // Scenario: Validation when values are missing
   if (!dividend.trim() || !divider.trim()) {
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    result.classList.add("error-message");
   }
 
   // Scenario: An invalid division should log an error in the console
   else if (divider.trim() === "0") {
     result.innerText = "Division not performed. Invalid number provided. Try again";
+    result.classList.add("error-message");
     throw new Error (
       "Invalid number provided, cannot use 0"
     );
